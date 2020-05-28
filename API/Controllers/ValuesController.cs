c62b63c278d0193bcb7e58c4b7647a4630d3ac2d
LewisNiblock <niblocklewis@gmail.com>
2020-05-28 01:47:34 +0100

    ddd

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private DataContext _context; 
+
+        public ValuesController(DataContext context)
+        {
+            _context = context; 
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<Value>>> Get(int id)
+        {
+            var values = await _context.Values.FindAsync(id); 
+            return Ok(values); 
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
+        {
+            var values = await _context.Values.ToListAsync(); 
+            return Ok(values); 
+        }
+
+    }
+}
